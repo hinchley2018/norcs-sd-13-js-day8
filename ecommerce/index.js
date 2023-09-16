c114ee1c3c87f50d2fb9ccff700d4ce7f79793d6
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2023-09-16 13:06:39 -0500

    connect pieces

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -1,5 +1,5 @@
 function searchProductList(searchTerm) {
-
+    console.log("searchTerm", searchTerm)
     let products = [
         {
             name: "Pedigree Dog food",
@@ -24,26 +24,23 @@ function searchProductList(searchTerm) {
     ]
 
     return new Promise(function (resolve, reject) {
-        resolve(products.filter(p => p.name.toLowerCase() === searchTerm.toLowerCase()))
+        //no search return home screen
+        if(searchTerm === undefined || searchTerm === ''){
+            resolve(products)
+        }
+        //they searched
+        else {
+            resolve(products.filter(p => p.name.toLowerCase() === searchTerm.toLowerCase()))
+        }
+        
     })
 }
 
-function getAvailableFilters() {
-    return new Promise((resolve, reject) => {
-        let availableFilters = {
-            brands: ["Blue ribbon", "Pedigree", "Amazon Basics"],
-            types: ["wet", "dry"],
-            minPrice: 0,
-            maxPrice: 600
-        }
-        resolve(availableFilters)
-    })
-}
-function displayFilters() { }
-function displayProducts() { }
 //build the page
 async function buildSearchPage() {
-    
+    //clear results
+    let productsContainer = document.querySelector("#products-container")
+    productsContainer.innerHTML = ""
     //get filters
     // let filters = await getAvailableFilters()
     // displayFilters(filters)
@@ -51,44 +48,24 @@ async function buildSearchPage() {
         brand: "Pedigree",
         type: "dry"
     }
-    // let products = await searchProductList(userSearchTerm, userFilters)
+    //gets my search input
+    let searchTermInput = document.querySelector("#product-search-input")
 
-    let products = searchProducts()
-    //assume I can draw on screen, how to get the products?
-    products.forEach(p => addProductCard(p.name,p.price))
+    let products = await searchProductList(searchTermInput.value)
+    
+    // No results for asdofijsdaoifjasdoifjiopjf.Try checking your spelling or use more general terms
+    if(products === null || products.length === 0){
+        //display error
+        let noResultsMessage = document.createElement("p")
+        noResultsMessage.textContent = "No results for: " + searchTermInput.value
+        productsContainer.append(noResultsMessage)
+    }else {
+        //assume I can draw on screen, how to get the products?
+        products.forEach(p => addProductCard(p.name,p.price))
+
+    }
 }
 buildSearchPage()
-{/* 
-<div class="product-card">
-    <h2 class="product-title">Blue ribbon dog food</h2>
-    <b><p>$59.99</p></b>
-</div> 
-*/}
-function searchProducts() {
-    let products = [
-        {
-            name: "Pedigree Dog food",
-            price: 8.94
-        },
-        {
-            name: "Pedigree wet dog food",
-            price: 8.94
-        },
-        {
-            name: "Blue ribbon Dog food",
-            price: 8.94
-        },
-        {
-            name: "macbook pro",
-            price: 2900
-        },
-        {
-            name: "potatoes",
-            price: 3.74
-        }
-    ]
-    return (products)
-}
 
 function messageStudents() {
     alert("This is much better, please use the pattern of js code in .js and html in html")
@@ -103,8 +80,11 @@ function messageStudents() {
 // console.log(document.querySelector("button"))
 // console.log(searchForm.children[1])
 
-// let searchButton = document.querySelector("#search-button")
-// console.log("selected by id",searchButton)
+let searchButton = document.querySelector("#search-button")
+console.log("selected by id",searchButton)
+searchButton.addEventListener('click',function (){
+    buildSearchPage()
+})
 
 // let productCards = document.querySelectorAll(".product-card")
 // console.log("selected by class", productCards)
@@ -123,6 +103,7 @@ function addProductCard(productName, productPrice) {
 
     //i'm assemblying an h2
     let productsContainer = document.querySelector("#products-container")
+
     console.log("conatiner", productsContainer)
 
     let productCard = document.createElement("div")
