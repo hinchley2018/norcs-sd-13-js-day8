ff4d2215136f9cfcc742f0cc6d217dd50c1c993b
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2023-09-16 09:24:35 -0500

    ecommerce/ and scripts

---
+++ ecommerce/index.js
@@ -0,0 +1,92 @@
+function searchProductList(searchTerm) {
+
+    let products = [
+        {
+            name: "Pedigree Dog food",
+            price: 8.94
+        },
+        {
+            name: "Pedigree wet dog food",
+            price: 8.94
+        },
+        {
+            name: "Blue ribbon Dog food",
+            price: 8.94
+        },
+        {
+            name: "macbook pro",
+            price: 2900
+        },
+        {
+            name: "potatoes",
+            price: 3.74
+        }
+    ]
+
+    return new Promise(function (resolve, reject) {
+        resolve(products.filter(p => p.name.toLowerCase() === searchTerm.toLowerCase()))
+    })
+}
+
+function getAvailableFilters(){
+    return new Promise((resolve, reject) => {
+        let availableFilters = {
+            brands: ["Blue ribbon", "Pedigree", "Amazon Basics"],
+            types: ["wet", "dry"],
+            minPrice: 0,
+            maxPrice: 600
+        }
+        resolve(availableFilters)
+    })
+}
+function displayFilters(){}
+function displayProducts() {}
+//build the page
+async function buildSearchPage() {
+    debugger
+    let userSearchTerm = prompt("Enter the product you want to search")
+    if (userSearchTerm === undefined) {
+        console.log("go to homescreen")
+    }
+    //get filters
+    let filters = await getAvailableFilters()
+    displayFilters(filters)
+    let userFilters = {
+        brand: "Pedigree",
+        type: "dry"
+    }
+    let products = await searchProductList(userSearchTerm,userFilters)
+    //assume I can draw on screen, how to get the products?
+    displayProducts(products)
+}
+
+
+function searchProducts(){
+    let products = [
+        {
+            name: "Pedigree Dog food",
+            price: 8.94
+        },
+        {
+            name: "Pedigree wet dog food",
+            price: 8.94
+        },
+        {
+            name: "Blue ribbon Dog food",
+            price: 8.94
+        },
+        {
+            name: "macbook pro",
+            price: 2900
+        },
+        {
+            name: "potatoes",
+            price: 3.74
+        }
+    ]
+    console.log(products)
+}
+
+function messageStudents(){
+    alert("This is much better, please use the pattern of js code in .js and html in html")
+}
